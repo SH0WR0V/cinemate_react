bb224f79ef25cb8b110ade8736d203bcaf7b89bc
SH0WR0V <sm.shahriar1230@gmail.com>
2024-05-05 02:15:15 +0600

    custom hook with dynamic route api fetch

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,18 +1,8 @@
-import { useEffect, useState } from "react";
 import { Card } from "../components/Card";
+import { useFetch } from "../hooks/useFetch";
 
-export const MovieList = () => {
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    async function fetchMovies() {
-      const response = await fetch(
-        "https://api.themoviedb.org/3/movie/now_playing?api_key=392412d812a35ebc4c1bc24dc2192894"
-      );
-      const data = await response.json();
-      setMovies(data.results);
-    }
-    fetchMovies();
-  }, []);
+export const MovieList = ({ apiPath }) => {
+  const { data: movies } = useFetch(apiPath);
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
