7d73d78298cdffcf5ca0766c980fae11355bc8b6
SH0WR0V <sm.shahriar1230@gmail.com>
2024-05-06 16:54:08 +0600

    search functionality added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import Logo from "../assets/logo.png";
 import { useState, useEffect } from "react";
 
 export const Header = () => {
   const [hidden, setHidden] = useState(true);
+  const navigate = useNavigate();
   const [darkMode, setDarkMode] = useState(
     JSON.parse(localStorage.getItem("darkMode")) || false
   );
@@ -22,6 +23,11 @@ export const Header = () => {
     "text-base block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500";
   const inActiveClass =
     "text-base block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
+
+  const handleChange = (e) => {
+    const queryTerm = e.target.value;
+    queryTerm === "" ? navigate("/") : navigate(`/search?q=${queryTerm}`);
+  };
 
   return (
     <header>
@@ -123,6 +129,7 @@ export const Header = () => {
                 className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Search..."
                 autoComplete="off"
+                onChange={handleChange}
               />
             </div>
             <button
